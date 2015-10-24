3fc114da4bca3e6c046c1aef953dcbc28d40505d
NamesAreStupid <kevin.harbarth@gmx.net>
2015-10-24 04:08:52 +0200

    added basic data binding

--- ChallengeOffline/mainController.js
+++ ChallengeOffline/mainController.js
@@ -1,4 +1,19 @@
 app.controller('mainController', ['$scope', function ($scope) {
-	$scope.money=800;
+	$scope.money='800';
+
+	$scope.user = {
+		usedWeapons : [],
+		faction : "ct",
+		money : 800,
+
+		//config Rules for filtering
+		filterUsed : true,
+		filterFaction : true,
+		filterPrice : true,
+		filterType : [],
+		filterCustom : []
+	};
+
 	$scope.weapons = weapons;
+
 }]);
